4dc0f5976d0e29bb46b7f6bc12e20dcde7c781bb
strike970124 <41593886+strike970124@users.noreply.github.com>
2019-08-04 07:05:18 -0500

    Terminado

--- queries/getSpanishCountries.sql
+++ queries/getSpanishCountries.sql
@@ -1,1 +1,8 @@
 -- Write the SQL Query to know how many countries have Spanish as their official language. Good luck!
+
+SELECT `countrylanguage`.`Language`,count(`countrylanguage`.`Language`) AS `#count`
+FROM `country`
+INNER JOIN `countrylanguage`
+ON `country`.`Code`=`countrylanguage`.`CountryCode`
+WHERE `countrylanguage`.`Language` = 'Spanish'
+GROUP BY `countrylanguage`.`Language`
